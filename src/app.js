6d5685622db23af2bc143f8642b697773a34108e
kashishchouhan13 <140608317+kashishchouhan13@users.noreply.github.com>
2025-02-02 22:08:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,13 +8,12 @@ const app=express();
 const port= process.env.PORT|| 4000;
 
 app.use(express.json());//express.json is a method inbuild in express to recognise the incoming 
-//Request Object as a JSON Object. This method is called middleware
+                        //Request Object as a JSON Object. This method is called middleware
 app.use(studentRouter);
 
 app.use(express.static(path.join(__dirname, "../")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"));
-    // res.send("Welcome to the CRUD API");
   });
 
 app.listen(port,()=>{

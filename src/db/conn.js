0c33261f0b411fb8a25b6938cea57cb4b5ddc4b0
kashishchouhan13 <140608317+kashishchouhan13@users.noreply.github.com>
2025-02-02 21:11:24 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
-
+require("dotenv").config();
 mongoose.connect(process.env.MONGO_URI || "mongodb://mongo:axnLEepoxaFDbYLyNRXRVWFbdqhdOYot@viaduct.proxy.rlwy.net:59723/students-api")
-// "mongodb://localhost:27017/students-api")
+  // "mongodb://localhost:27017/students-api")
 .then(()=>console.log("connection sucessful"))
 .catch((err)=>console.log(err));

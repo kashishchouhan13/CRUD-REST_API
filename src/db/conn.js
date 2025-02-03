a4570d5a9449db1c682b81c97948a011c1c8be21
kashishchouhan13 <140608317+kashishchouhan13@users.noreply.github.com>
2025-02-03 15:58:22 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
-mongoose.connect(process.env.MONGO_URL||mongodb+srv://kashish:kashish13@cluster.ids5y.mongodb.net/students-api?retryWrites=true&w=majority&appName=Cluster )
+mongoose.connect(process.env.MONGO_URL||"mongodb+srv://kashish:kashish13@cluster.ids5y.mongodb.net/students-api?retryWrites=true&w=majority&appName=Cluster" )
   // ||"mongodb://mongo:axnLEepoxaFDbYLyNRXRVWFbdqhdOYot@viaduct.proxy.rlwy.net:59723/students-api")
   // "mongodb://localhost:27017/students-api")
 .then(()=>console.log("connection sucessful"))
